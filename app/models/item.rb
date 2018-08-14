f3ce3ccb6b3ff94d898327856474905a28ac6910
david mcginn <mcginn@hotmail.com>
2018-08-14 17:46:20 +0000

    updated html to suit gofox

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   has_many :members, through: :orders
 
   validates :name, presence: true
-  validates :category, presence: true
+  #validates :category, presence: true
 end
