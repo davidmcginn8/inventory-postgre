7b4da6fd33a6c61c8d80d42028b71b3a3bd50b59
david mcginn <mcginn@hotmail.com>
2018-08-27 12:56:14 +0000

    updated login form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-User.delete_all
+#User.delete_all
 #User.create([{ name: 'Zacharias', email: 'demo@demo.com', password: "change_me", remember_created_at: nil }])
-User.create([{ name: 'Dave', email: 'd.mcginn@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
-User.create([{ name: 'Lee', email: 'l.russell@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
-User.create([{ name: 'Anthony', email: 'a.kelly@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
-User.create([{ name: 'Conor', email: 'c.oloughlin@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
-User.create([{ name: 'Timmy', email: 't.fisher@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
-User.create([{ name: 'Finn', email: 'finn@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Dave', email: 'd.mcginn@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Lee', email: 'l.russell@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Anthony', email: 'a.kelly@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Conor', email: 'c.oloughlin@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Timmy', email: 't.fisher@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+#User.create([{ name: 'Finn', email: 'finn@glofox.com', password: "HelloWorld123", remember_created_at: nil }])
+
+#User.crete([{ name: '',category: '', quantity: 1, description: '', created_at: '', updated_at: '', remaining_quantity: ''}])
+
+#Items.crete([{ name: 'Seed_test', category: 'Book', quantity: 1, description: 'Seed Description', created_at: 2001-02-03T04:05:06+00:00, updated_at: 2001-02-03T04:05:06+00:00, remaining_quantity: 2001-02-03T04:05:06+00:00 }])
+
 
